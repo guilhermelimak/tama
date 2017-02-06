9dbd93eb3ba67ff1126cf8f1b86f60ae598f8478
Guilherme Lima <guilhermelimak@gmail.com>
2017-02-05 16:04:21 -0800

    Add split and capitalize helper

---
+++ src/util/splitAndCapitalize.js
@@ -0,0 +1,10 @@
+/**
+ * Split camel case names and capitalize the letters
+ *
+ * @method  splitAndCapitalize
+ * @param    {String}      string   Name to be split
+ * @return   {String}      Splitted and capitalized name
+ */
+export default function splitAndCapitalize(string) {
+  return string.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase())
+}
