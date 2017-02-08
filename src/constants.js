cde66c1af2c77fab7f871d6d87292f69e204ff7c
Guilherme Lima <guilhermelimak@gmail.com>
2017-02-07 23:08:34 -0200

    Add timestamp regex

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const TIMESTAMP_FORMAT = 'hh:mm:ss YYYY.MM.DD'
+export const TIMESTAMP_REGEX = /([0-9]{2}:){2}[0-9]{2}\s[0-9]{4}\.[0-9]{2}\.[0-9]{2}/
--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import Client from 'src/Client'
-import Server from 'src/Server'
-
-export default {
-  Client,
-  Server,
-}
+export { default as Client } from './Client'
+export { default as Server } from './Server'
