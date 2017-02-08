ca970bee32bc78dc6036194ffb759e7a98f3d174
Guilherme Lima <guilhermelimak@gmail.com>
2017-02-08 20:23:41 -0200

    Update log event function

--- src/util/logEvent.js
+++ src/util/logEvent.js
@@ -1,10 +1,11 @@
-export const logMessage = (message) => {
-  if (!message.meta) return message
+export default (message) => {
+  if (!message || !message.meta) return message
   console.log('============================================='.green)
-  console.log('| Event: '.yellow + `${message.type}`.underline.blue)
-  console.log('| at:    '.yellow + `${message.meta.timestamp}`.underline.blue)
-  console.log('| from:  '.yellow + `${message.meta.publisher}`.underline.blue)
-  console.log('| to:    '.yellow + `${message.meta.recipient}`.underline.blue)
+  console.log('| type:       '.yellow + `${message.type}`.underline.blue)
+  console.log('| timestamp:  '.yellow + `${message.meta.timestamp}`.underline.blue)
+  console.log('| publisher:  '.yellow + `${message.meta.publisher}`.underline.blue)
+  console.log('| recipient:  '.yellow + `${message.meta.recipient}`.underline.blue)
+  console.log('| payload:    '.yellow + `${JSON.stringify(message.payload)}`.underline.blue)
   console.log('============================================='.green)
 
   return message
