de2390d99bff82c6ffb51aed8738fa7760608f59
Guilherme Lima <guilhermelimak@gmail.com>
2017-02-09 00:02:36 -0200

    Update index exports tests

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -1,7 +1,14 @@
-import rem from '../src'
+import { Client, Server } from '../src'
 
 describe('index file exports', () => {
   it('should containg the Server and Client properties', () => {
+    expect(Client).toBeDefined()
+    expect(Server).toBeDefined()
+  })
+
+  it('should be able to use it with require()', () => {
+    const rem = require('../src')
+
     expect(rem.Client).toBeDefined()
     expect(rem.Server).toBeDefined()
   })
