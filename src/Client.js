50fdabd4fde1f6297d16c4edcacbd9a7ab2c629c
Guilherme Lima <guilhermelimak@gmail.com>
2017-02-08 23:07:11 -0200

    Add some Client improvements
    Add client docs.
    Change client constructor arguments.
    Retry message when the register event was not yet received and the instance not fully initialized.
    Formatting

--- src/Client.js
+++ src/Client.js
@@ -5,30 +5,66 @@ import Event from 'src/shared/Event'
 import parseMessage from 'src/shared/parseMessage'
 import clientHandlers from 'src/client/clientHandlers'
 
+const RETRY_INTERVAl = 1000
 const DEFAULT_URL = 'ws://localhost:9000'
 
 let instance = null
 
 export default class RemClient {
-  constructor(customOptions, handlers) {
+  /**
+   * Create a new client instance
+   *
+   * @method   constructor
+   *
+   * @param    {String}      url             Server url to connect
+   * @param    {Array}       handlers        Handlers array
+   *
+   * @return   {Client}      Client instance
+   */
+  constructor(url = DEFAULT_URL, handlers = []) {
     if (!instance) instance = this
-
-    const defaultOptions = { url: DEFAULT_URL }
-    const options = Object.assign(defaultOptions, customOptions)
     const _handlers = [...handlers, ...clientHandlers]
 
-    this.ws = new WebSocket(options.url)
+    this.ws = new WebSocket(url)
     this.ws.on('open', () => console.log('Connected to server'))
     this.ws.on('message', message => (parseMessage.bind(this)(message, _handlers)))
 
     return instance
   }
 
+
+  /**
+   * Emit event to the server. This method will retry the emitEvent forever in 1s intervals until
+   * it succeeds
+   *
+   * @method   emitEvent
+   *
+   * @param    {String}                 type      Event type
+   * @param    {Object|Array|String}    payload   Event payload
+   */
+  /* eslint-disable class-methods-use-this */
   emitEvent(type, payload) {
-    const meta = { publisher: this.identifier, recipient: 'srv' }
-    const event = new Event({ type, payload, meta }).log()
-    this.ws.send(event.toString())
+    // VAI SI FUDE TODO MUNDO DESGRAÇA
+    let eventEmitted = false
+
+    setInterval(() => {
+      if (!this || !this.identifier || eventEmitted) return
+
+      const event = new Event({
+        type,
+        payload,
+        meta: {
+          publisher: this.identifier,
+          recipient: 'srv',
+        },
+      }).log()
+
+      this.ws.send(event.toString())
+
+      eventEmitted = true
+    }, RETRY_INTERVAl)
   }
+
 
   get identifier() { return this._identifier }
   set identifier(val) { this._identifier = val }
