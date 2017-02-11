16195641a2776ead0c62fba39475cc159b1ce134
Guilherme Lima <guilhermelimak@gmail.com>
2017-02-11 03:32:26 -0200

    Rename message to event in logEvent

--- src/util/logEvent.js
+++ src/util/logEvent.js
@@ -1,14 +1,14 @@
 import 'colors'
 
-export default (message) => {
-  if (!message || !message.meta) return message
+export default (event) => {
+  if (!event || !event.meta) return event
   console.log('============================================='.green)
-  console.log('| type:       '.yellow + `${message.type}`.underline.blue)
-  console.log('| timestamp:  '.yellow + `${message.meta.timestamp}`.underline.blue)
-  console.log('| publisher:  '.yellow + `${message.meta.publisher}`.underline.blue)
-  console.log('| recipient:  '.yellow + `${message.meta.recipient}`.underline.blue)
-  console.log('| payload:    '.yellow + `${JSON.stringify(message.payload)}`.underline.blue)
+  console.log('| type:       '.yellow + `${event.type}`.underline.blue)
+  console.log('| timestamp:  '.yellow + `${event.meta.timestamp}`.underline.blue)
+  console.log('| publisher:  '.yellow + `${event.meta.publisher}`.underline.blue)
+  console.log('| recipient:  '.yellow + `${event.meta.recipient}`.underline.blue)
+  console.log('| payload:    '.yellow + `${JSON.stringify(event.payload)}`.underline.blue)
   console.log('============================================='.green)
 
-  return message
+  return event
 }
