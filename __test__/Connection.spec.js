b2897a2e8f55e919289fb66a4d26071477b70681
Guilherme Lima <guilhermelimak@gmail.com>
2017-02-11 03:50:03 -0200

    Add connection tests

---
+++ __test__/Connection.spec.js
@@ -0,0 +1,16 @@
+import Connection from 'src/server/Connection'
+
+describe('Connection', () => {
+  it('should assign the props passed as arguments', () => {
+    const opt = {
+      ip: 'testIp',
+      id: 'testId',
+      socket: 'testSocket',
+    }
+
+    const connection = new Connection(opt)
+    expect(connection.ip).toBe(opt.ip)
+    expect(connection.id).toBe(opt.id)
+    expect(connection.sockeet).toBe(opt.sockeet)
+  })
+})
