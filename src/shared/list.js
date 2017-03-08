6497d745421be13277910596abac5992eca0dab6
Guilherme Lima <guilhermelimak@gmail.com>
2017-03-08 01:11:38 -0300

    feat(list): Add parameter to initialize list

--- src/shared/list.js
+++ src/shared/list.js
@@ -2,9 +2,9 @@
  * List used to hold objects
  */
 export default class List {
-  constructor(idKey = 'id') {
-    this.idKey = idKey
-    this.items = []
+  constructor(items = [], idKey = 'id') {
+    this._idKey = idKey
+    this.items = items
   }
 
   /**
@@ -21,7 +21,15 @@ export default class List {
    * @method remove
    * @param  {Object}  itemId   Id of item instance to be deleted
    */
-  remove(itemId) { if (itemId) this.items = this.items.filter(i => i[this.idKey] !== itemId) }
+  remove(itemId) { if (itemId) this.items = this.items.filter(i => i[this._idKey] !== itemId) }
+
+  /**
+   * Get item by itemId
+   * @method item
+   * @param  {String|Number} itemId [description]
+   * @returns {Object} [description]
+   */
+  item(itemId) { return this._items.find(i => i[this._idKey] === itemId) || undefined }
 
   get items() { return this._items }
   set items(val) { this._items = val }
