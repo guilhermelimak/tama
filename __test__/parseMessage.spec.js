d5a01f66cf73bac37c1f845a205537238ed28494
Guilherme Lima <guilhermelimak@gmail.com>
2017-03-09 01:25:51 -0300

    test(parseMessage): Fix parseMessage tests

--- __test__/parseMessage.spec.js
+++ __test__/parseMessage.spec.js
@@ -3,10 +3,18 @@ import dummyEvent from '__test__/util/dummyEvent'
 
 describe('parseMessage', () => {
   it('should parse the string message and create a new event', () => {
+    const handler = jest.fn()
     const handlers = [{
       type: 'testEvent',
-      handler() {},
+      handler,
     }]
+
     parseMessage(dummyEvent().toString(), handlers)
+    expect(handler.mock.calls.length).toBe(1)
+  })
+
+  it('should return undefined when event is not found in handlers list', () => {
+    const result = parseMessage(dummyEvent().toString(), [])
+    expect(result).not.toBeDefined()
   })
 })
